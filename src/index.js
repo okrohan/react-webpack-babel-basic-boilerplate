b0fcb4601aa6be4341c1298e9973d32b3ccd4a53
Rohan Salunke <rohans16996@gmail.com>
2019-04-11 20:59:23 +0530

    Initialize Basic Components

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function HelloWorlComponent () {
+  return (<div>Hello This is a minimal React BoilerPlate</div>)
+}
+ReactDOM.render(<HelloWorlComponent/>, document.getElementById("container"));
